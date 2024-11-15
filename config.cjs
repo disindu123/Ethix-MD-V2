2439ad54b303abe45afb6d1c454aa7fea9f27094
disindu123 <disindudamsandu@gmail.com>
2024-11-15 15:25:21 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,8 +3,9 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&DUmJNurv",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&eH28Uzbt",
   PREFIX: process.env.PREFIX || '.',
+  COMMAND_TYPE: process.env.COMMAND_TYPE || 'nonbutton',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY !== undefined ? process.env.AUTO_STATUS_REPLY === 'true' : true,
   STATUS_READ_MSG: process.env.STATUS_READ_MSG || '',
